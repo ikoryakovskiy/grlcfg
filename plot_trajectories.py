4807ca4944c962b9cb0812b7f29813cd588c46a6
Ivan Koryakovskiy <i.koryakovskiy@tudelft.nl>
2017-03-10 17:22:35 +0100

    Let's try walkdynamic on leo

--- plot_trajectories.py
+++ plot_trajectories.py
@@ -11,28 +11,37 @@ from enum import Enum
 import pylab
 from os.path import basename
 
+sys.path.append('/home/ivan/work/scripts/py')
+from my_csv.utils import *
+
 class ELeoJoint(Enum):
     ljHipLeft, ljHipRight, ljKneeLeft, ljKneeRight, ljAnkleLeft, ljAnkleRight, ljShoulder, ljTorso = range(8)
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('filename')
+    parser.add_argument('file', nargs='+')
+
     args = parser.parse_args()
 
-    data = np.loadtxt(args.filename, skiprows=46, delimiter=',')
-    ts = data[:, 0]       # time
-    xs = data[:, 1:17]    # states
-    xn = data[:, 17:33]   # next states
-    xc = data[:, 33:40]   # controls
+    dd = []
+    for i, f in enumerate(args.file):
+      hd_sz = get_header_size(f)
+      data = np.loadtxt(f, skiprows=hd_sz, delimiter=',')
+      ts = data[:, 0]       # time
+      xs = data[:, 1:17]    # states
+      xn = data[:, 17:33]   # next states
+      xc = data[:, 33:40]   # controls
+      dd.append({'ts':ts, 'xs':xs, 'xn':xn, 'xc':xc})
 
     plt.close('all')
 
-    bn = os.path.splitext(basename(args.filename))[0]
+    bn = '' #os.path.splitext(basename(args.filename))[0]
 
     f, axarr = plt.subplots(8, sharex=True)
     for i in ELeoJoint:
-      axarr[i.value].plot(ts, xs[:, i.value])
-      axarr[i.value].plot(ts, np.repeat(np.average(xs[:, i.value]), len(ts)))
+      for d in dd:
+        axarr[i.value].plot(d['ts'], d['xs'][:, i.value])
+      #axarr[i.value].plot(ts, np.repeat(np.average(xs[:, i.value]), len(ts)))
       aname = str(i).rsplit('.', 1)[-1]
       axarr[i.value].set_ylabel(aname)
     axarr[0].set_title('pos-'+bn)
@@ -41,7 +50,8 @@ def main():
 
     f, axarr = plt.subplots(8, sharex=True)
     for i in ELeoJoint:
-      axarr[i.value].plot(ts, xs[:, 8+i.value])
+      for d in dd:
+        axarr[i.value].plot(d['ts'], d['xs'][:, 8+i.value])
       aname = str(i).rsplit('.', 1)[-1]
       axarr[i.value].set_ylabel(aname)
     axarr[0].set_title('vel-'+bn)
@@ -52,7 +62,8 @@ def main():
     f, axarr = plt.subplots(7, sharex=True)
     for i in ELeoJoint:
       if i.value < 7:
-        axarr[i.value].plot(ts, xc[:, i.value])
+        for d in dd:
+          axarr[i.value].plot(d['ts'], d['xc'][:, i.value])
         aname = str(i).rsplit('.', 1)[-1]
         axarr[i.value].set_ylabel(aname)
     axarr[0].set_title('u-'+bn)
