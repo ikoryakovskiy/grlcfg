10e3c118b068e1c5c212ad80b517e16efbd02106
Ivan Koryakovskiy <i.koryakovskiy@tudelft.nl>
2017-03-16 18:47:18 +0100

    Tested walkdynamic again

---
+++ pendulum/sample_param.py
@@ -0,0 +1,18 @@
+from __future__ import division
+import multiprocessing
+import os
+import os.path
+import sys
+import yaml, collections
+import numpy as np
+from time import sleep
+import math
+import argparse
+
+
+def main():
+    sss
+
+if __name__ == "__main__":
+    main()
+
