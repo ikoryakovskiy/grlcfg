72419acd622d9f9d2a74fb696cd8022f2de817cb
Ivan Koryakovskiy <i.koryakovskiy@tudelft.nl>
2017-04-03 09:32:19 +0200

    Running it0

--- pendulum/sarsa_grid_results/bayes.py
+++ pendulum/sarsa_grid_results/bayes.py
@@ -38,21 +38,21 @@ def main():
   else:
       args.cores = min(multiprocessing.cpu_count(), 32)
   print('Using {} cores.'.format(args.cores))
-    
+
   ##############################################
   #cma_test()
-  #mp_cma_test  
+  #mp_cma_test
   ##############################################
-  
+
   # Import data
   n = 50
   size  = (125, 101, 3)
   dsize = (4, 4, 3)
-  
+
   offset = size[0]*size[1]
   num = np.prod(size)
   dnum = np.prod(dsize)
-  
+
   train = np.zeros((n, num))
   for i in range(0, n):
     train[i] = load_grid_representation("data/cfg_pendulum_sarsa_grid-{:03d}-mp0-run0-_experiment_agent_policy_representation.dat".format(i))
@@ -61,38 +61,39 @@ def main():
     #plt.waitforbuttonpress()
     #show_grid_representation(train[i], (0, 1), (125, 101, 3))
     #plt.waitforbuttonpress()
-  
-  
+
+
+
   tm = train.mean(0)
   ts = train.std(0)
   tv2 = 2*np.maximum( num * [0.0001], train.var(0))
-  
+
   save_grid_representation(tm, "policies/cfg_pendulum_sarsa_grid-init-mp0-run0-_experiment_agent_policy_representation.dat")
 
   Q_init = tm
-  
+
   Q_hat = mp_cma_run(args, Q_init, size, dsize)
   Q_hat.tofile("q_hat.bin")
-  
+
   save_grid_representation(tm, "policies/cfg_pendulum_sarsa_grid-it0-mp0-run0-_experiment_agent_policy_representation.dat")
 
   for i in range(0, 3):
     show_grid_representation(Q_init[offset*i:offset*(i+1)], (0, 1), (125, 101, 1))
     show_grid_representation(Q_hat[offset*i:offset*(i+1)], (0, 1), (125, 101, 1))
   plt.waitforbuttonpress()
-  
+
   a
-  
-  
+
+
   csv_data = csv_read(["trajectories/pendulum_sarsa_grid_play-test-0.csv"])
   tr = load_trajectories(csv_data)
-  
+
   targets = real_targets(tm, tr, 0.97)
   #targets = np.zeros([1, 4])
   #print("Targets ", targets)
   #print(targets.shape[0])
-  
-  
+
+
   #print(ts)
   tsx = np.maximum(ts, 0.0000001)
   init_state = tm#np.random.normal(tm, tsx)
@@ -101,26 +102,26 @@ def main():
   #plt.waitforbuttonpress()
   print("Initial state", init_state)
   print("Min of the state {}".format(np.amin(init_state)))
-  
+
   optimizer = OptimizerSA(init_state, targets, tm, ts, tv2)
   state, e = optimizer.anneal()
   print("Error {}".format(e))
-  
+
   show_grid_representation(state, (0, 1), (125, 101, 3))
   plt.waitforbuttonpress()
-  
+
   save_grid_representation(state, "policies/cfg_pendulum_sarsa_grid-it1-mp0-run0-_experiment_agent_policy_representation.dat")
-  
+
   #for i in range(0, 3):
   #  show_grid_representation(state[offset*i:offset*(i+1)], (0, 1), (125, 101, 1))
   #  plt.waitforbuttonpress()
-  
-  
-  
+
+
+
   #policy = calc_grid_policy(tm, (0, 1), (125, 101, 3))
   #show_grid_representation(policy, (0, 1), (125, 101, 1))
   #plt.waitforbuttonpress()
-  
+
   #for i in range(0, 3):
   #  show_grid_representation(tm[offset*i:offset*(i+1)], (0, 1), (125, 101, 1))
   #  plt.waitforbuttonpress()
@@ -131,18 +132,18 @@ def main():
 def mp_cma_run(args, Q_init, size, dsize):
   if (size[2] != dsize[2]):
     raise ValueError('CMAES::init Dimensions are not correct')
-  
+
   offset = size[0]*size[1]
   actions = size[2]
-  
+
   q_inits = []
   for i in range(actions):
     q_inits.append(Q_init[offset*i:offset*(i+1)])
-    
+
   mp_size = (size[0], size[1], 1)
   mp_dsize = (dsize[0], dsize[1], 1)
   q_hats = do_multiprocessing_pool(args, q_inits, mp_size, mp_dsize)
-  
+
   Q_hat = np.empty(Q_init.shape)
   for i in range(actions):
     Q_hat[offset*i:offset*(i+1)] = q_hats[i]
@@ -152,50 +153,50 @@ def mp_cma_run(args, Q_init, size, dsize):
 def cma_test():
   size  = (125, 101, 1)
   dsize = (3, 2, 1)
-  
+
   cmaes = CMAES(size, dsize)
-  
+
   f_true = np.array([[0, 500, 0, 0, 0, -500]], dtype='float64')
   q_init_ref = cmaes.evaluate(f_true)
   q_init = np.copy(q_init_ref)
-  
+
   cmaes = CMAES(size, dsize)
   f_init = cmaes.initial(q_init)
   print(f_init)
   f_hat = cmaes.optimize(q_init, f_init)
   q_hat = cmaes.evaluate(f_hat[0])
   print(f_hat[0], f_hat[1])
-   
+
   print(cmaes.objective(np.array(f_true)))
   show_grid_representation(q_init, (0, 1), (size[0], size[1], 1))
   show_grid_representation(q_hat, (0, 1), (size[0], size[1], 1))
-  
+
   plt.waitforbuttonpress()
-  
+
 ######################################################################################
 def mp_cma_test(args):
   size  = (125, 101, 1)
   dsize = (3, 2, 1)
-  
+
   cmaes = CMAES(size, dsize)
-  
+
   f_trues = []
   f_trues.append(np.array([[-500, 500, -500, 500, -500, 500]], dtype='float64'))
   f_trues.append(np.array([[-500, 0, 0, 0, 0, 500]], dtype='float64'))
-  
+
   q_inits = []
   for i in range(2):
     q_inits_ref = cmaes.evaluate(f_trues[i])
-    q_inits.append(np.copy(q_inits_ref)) 
-  
+    q_inits.append(np.copy(q_inits_ref))
+
   q_hats = do_multiprocessing_pool(args, q_inits, size, dsize)
-   
+
   for i in range(2):
     cmaes.q = q_inits[i]
     print(cmaes.objective(np.array(f_trues[i])))
     show_grid_representation(q_inits[i], (0, 1), (size[0], size[1], 1))
     show_grid_representation(q_hats[i], (0, 1), (size[0], size[1], 1))
-  
+
   plt.waitforbuttonpress()
 
 ######################################################################################
@@ -208,7 +209,7 @@ def mp_run(size, dsize, q_init):
   q_hat = cmaes.evaluate(f_hat[0])
   print(f_hat[0], f_hat[1])
   return q_hat
-    
+
 ######################################################################################
 def do_multiprocessing_pool(args, q_initss, size, dsize):
   """Do multiprocesing"""
--- pendulum/sarsa_grid_results/show_results.py
+++ pendulum/sarsa_grid_results/show_results.py
@@ -23,55 +23,18 @@ from my_csv.utils import *
 dim = (125, 101, 3)
 offset = dim[0]*dim[1]
 
-# Experimnet with 500 iterations
-# Temperature        Energy    Accept   Improve     Elapsed   Remaining
-#     0.00100        572.16     2.88%     1.81%     0:14:06     0:00:00
-
-#with open("2017-03-27T22h48m49s_energy_610.201649024.state", 'rb') as fh:
-#    state1 = pickle.load(fh)
-    
-#with open("2017-03-27T23h22m16s_energy_572.163801119.state", 'rb') as fh:       # no cond
- #   state1 = pickle.load(fh)
-
-#with open("2017-03-27T23h04m28s_energy_572.832753648.state", 'rb') as fh:       # no cond
-#    state2 = pickle.load(fh)
-
-# Temperature        Energy    Accept   Improve     Elapsed   Remaining
-#     0.00100        548.64     2.43%     1.39%     0:29:59     0:00:00
-#with open("2017-03-28T08h58m54s_energy_548.635027241.state", 'rb') as fh:       # no cond
-#    state1 = pickle.load(fh)
-
-#with open("2017-03-28T10h06m24s_energy_548.773715401.state", 'rb') as fh:       # no cond
-#    state2 = pickle.load(fh)
-
-# Temperature        Energy    Accept   Improve     Elapsed   Remaining
-#     0.00001        527.93     0.46%     0.41%     0:28:08     0:00:00
-#with open("2017-03-28T11h51m57s_energy_527.65603926.state", 'rb') as fh:       # no cond
-#    state1 = pickle.load(fh)
-
-#with open("2017-03-28T12h31m50s_energy_527.933218316.state", 'rb') as fh:       # no cond
-#    state2 = pickle.load(fh)
-
-
-#with open("2017-03-28T15h59m55s_energy_2959.65632801.state", 'rb') as fh:       # no cond
-#    state1 = pickle.load(fh)
-
-#with open("2017-03-28T16h59m08s_energy_2968.9286122.state", 'rb') as fh:       # no cond
-#    state2 = pickle.load(fh)
-
-
-with open("2017-03-28T19h12m30s_energy_518.56423151.state", 'rb') as fh:       # no cond
-    state1 = pickle.load(fh)
-
-with open("2017-03-28T20h19m45s_energy_516.746097874.state", 'rb') as fh:       # no cond
-    state2 = pickle.load(fh)
+q0 = np.fromfile("policies/cfg_pendulum_sarsa_grid-it0-mp0-run0-_experiment_agent_policy_representation.dat")
 
 csv_data = csv_read(["trajectories/pendulum_sarsa_grid_play-test-0.csv"])
 tr = load_trajectories(csv_data)
-#targets = real_targets(tm, tr, 0.97)
 
-policy1 = calc_grid_policy(state1, (0, 1), (125, 101, 3))
-show_grid_representation(policy1, (0, 1), (125, 101, 1))
+for i in range(0, 3):
+  show_grid_representation(q0[offset*i:offset*(i+1)], (0, 1), (125, 101, 1))
+  plt.scatter(tr[:,0], tr[:,1], c='k', s=40, marker='+')
+  plt.waitforbuttonpress()
+
+p0 = calc_grid_policy(q0, (0, 1), (125, 101, 3))
+show_grid_representation(p0, (0, 1), (125, 101, 1))
 plt.scatter(tr[:,0], tr[:,1], c='w', s=40, marker='+')
 plt.waitforbuttonpress()
 
@@ -83,7 +46,7 @@ plt.waitforbuttonpress()
 policy = policy1-policy2
 show_grid_representation(policy, (0, 1), (125, 101, 1))
 plt.scatter(tr[:,0], tr[:,1], c='w', s=40, marker='+')
-plt.waitforbuttonpress() 
+plt.waitforbuttonpress()
 
 
 '''
