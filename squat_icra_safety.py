ef3e723b18f28f19f211eeb3b79aad3ea8a7e197
Ivan Koryakovskiy <i.koryakovskiy@tudelft.nl>
2017-11-22 13:55:57 +0100

    One iteration of CAL safety

--- squat_icra_safety.py
+++ squat_icra_safety.py
@@ -62,11 +62,11 @@ def main():
                 #
                 #"leo/icra/rbdl_nmpc_2dpg_squat_fb_sl_fa_vc.yaml",
                 #"leo/icra/rbdl_nmpc_2dpg_squat_fb_sl_vc.yaml",
-#                "leo/icra/rbdl_nmpc_2dpg_ou_squat_fb_sl_vc.yaml",
+                "leo/icra/rbdl_nmpc_2dpg_ou_squat_fb_sl_vc.yaml",
                 #
                 #"leo/icra/rbdl_nmpc_2dpg_squat_fb_sl_fa_vc_mef.yaml",
                 #"leo/icra/rbdl_nmpc_2dpg_squat_fb_sl_vc_mef.yaml",
-                "leo/icra/rbdl_nmpc_2dpg_ou_squat_fb_sl_vc_mef.yaml",
+#                "leo/icra/rbdl_nmpc_2dpg_ou_squat_fb_sl_vc_mef.yaml",
               ]
 
     
